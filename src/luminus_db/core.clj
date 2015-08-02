58d5ed25d0dcc40bd5cdb7b4acf6024c126cc94f
Yogthos <dmitri.sotnikov@gmail.com>
2015-08-02 09:55:14 -0400

    added bind-connection and with-transaction macros

--- src/luminus_db/core.clj
+++ src/luminus_db/core.clj
@@ -3,25 +3,23 @@
             [clj-dbcp.core :as dbcp]
             [clojure.java.jdbc :as jdbc]))
 
-(defn- queries-ns-name []
-  (str (name (ns-name *ns*)) ".connectionless-queries"))
-
-(defn init! [conn & filenames]
-  (let [base-namespace *ns*
-        queries-ns (symbol (queries-ns-name))]
-    (in-ns queries-ns)
-    (require '[yesql.core :as yesql])
-    (doseq [filename filenames]
-      (let [yesql-queries (yesql/defqueries filename)]
-        (doall
-         (for [yesql-query yesql-queries]
-           (intern base-namespace
-                   (with-meta (:name (meta yesql-query)) (meta yesql-queries))
+(defmacro bind-connection
+  [conn & filenames]
+  `(let [base-namespace# *ns*
+         queries-ns# (-> *ns* ns-name name (str ".connectionless-queries") symbol)]
+     (create-ns queries-ns#)
+     (in-ns queries-ns#)
+     (require 'yesql.core)
+     (doseq [filename# ~(vec filenames)]
+       (let [yesql-queries# (yesql.core/defqueries filename#)]
+         (doseq [yesql-query# yesql-queries#]
+           (intern base-namespace#
+                   (with-meta (:name (meta yesql-query#)) (meta yesql-queries#))
                    (fn
-                     ([] (yesql-query {} {:connection @conn}))
-                     ([args] (yesql-query args {:connection @conn}))
-                     ([args conn] (yesql-query args {:connection conn}))))))))
-    (in-ns (ns-name base-namespace))))
+                     ([] (yesql-query# {} {:connection (deref ~conn)}))
+                     ([args#] (yesql-query# args# {:connection (deref ~conn)}))
+                     ([args# conn#] (yesql-query# args# {:connection conn#})))))))
+     (in-ns (ns-name base-namespace#))))
 
 (defn connect! [conn pool-spec]
   (try
@@ -36,3 +34,12 @@
     (when-not (.isClosed ds)
       (.close ds)
       (reset! conn nil))))
+
+(defmacro with-transaction
+  "runs the body in a transaction where t-conn is the name of the transaction connection
+   the body will be evaluated within a binding where conn is set to the transactional
+   connection"
+  [args & body]
+  `(jdbc/with-db-transaction [~(first args) (deref ~(second args))]
+     (binding [~(second args) (atom ~(first args))]
+       ~@body)))
