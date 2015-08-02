35b17015a583d68d892c016f79fade35e2041248
Yogthos <dmitri.sotnikov@gmail.com>
2015-08-01 20:01:38 -0400

    updated to allow connection per namespace

--- src/luminus_db/core.clj
+++ src/luminus_db/core.clj
@@ -1,27 +1,29 @@
 (ns luminus-db.core
   (:require [yesql.core :as yesql]
             [clj-dbcp.core :as dbcp]
-            [clojure.java.jdbc :as jdbc]
-            [to-jdbc-uri.core :refer [to-jdbc-uri]]))
+            [clojure.java.jdbc :as jdbc]))
 
-(defonce ^:dynamic conn (atom nil))
+(defn- queries-ns-name []
+  (str (name (ns-name *ns*)) ".connectionless-queries"))
 
-(defn connect! [url pool-spec]
-  (try
-    (reset!
-      conn
-      {:datasource
-       (dbcp/make-datasource
-         (assoc pool-spec
-           :jdbc-url (to-jdbc-uri url)))})
-    (catch Throwable t
-      (throw (Exception. "Error occured while connecting to the database!" t)))))
-
-(defn disconnect! []
-  (when-let [ds (:datasource @conn)]
-    (when-not (.isClosed ds)
-      (.close ds)
-      (reset! conn nil))))
+(defn init! [filename]
+  (let [conn* (atom nil)
+        base-namespace *ns*
+        queries-ns (symbol (queries-ns-name))]
+    (in-ns queries-ns)
+    (require '[yesql.core :as yesql])
+    (defonce ^:dynamic conn conn*)
+    (let [yesql-queries (yesql/defqueries filename)]
+      (doall
+       (for [yesql-query yesql-queries]
+         (intern base-namespace
+                 (with-meta (:name (meta yesql-query)) (meta yesql-queries))
+                 (fn
+                   ([] (yesql-query {} {:connection @conn}))
+                   ([args] (yesql-query args {:connection @conn}))
+                   ([args conn] (yesql-query args conn))))))
+      (in-ns (ns-name base-namespace))
+      conn*)))
 
 (defmacro with-transaction [t-conn & body]
   `(jdbc/with-db-transaction
@@ -29,20 +31,16 @@
      (binding [luminus-db.core/conn (atom ~t-conn)]
        ~@body)))
 
-(defn defqueries [filename]
-  (let [base-namespace *ns*
-        queries-ns (symbol (str (name (ns-name *ns*)) ".connectionless-queries"))]
-    (create-ns queries-ns)
-    (in-ns queries-ns)
-    (require '[yesql.core :as yesql])
-    (let [yesql-queries (yesql/defqueries filename)
-          queries (doall
-                   (for [yesql-query yesql-queries]
-                     (intern base-namespace
-                             (with-meta (:name (meta yesql-query)) (meta yesql-queries))
-                             (fn
-                               ([] (yesql-query {} {:connection @conn}))
-                               ([args] (yesql-query args {:connection @conn}))
-                               ([args conn] (yesql-query args conn))))))]
-      (in-ns (ns-name base-namespace))
-      queries)))
+(defn connect! [conn pool-spec]
+  (try
+    (reset!
+     conn
+     {:datasource (dbcp/make-datasource pool-spec)})
+    (catch Throwable t
+      (throw (Exception. "Error occured while connecting to the database!" t)))))
+
+(defn disconnect! [conn]
+  (when-let [ds (:datasource @conn)]
+    (when-not (.isClosed ds)
+      (.close ds)
+      (reset! conn nil))))
