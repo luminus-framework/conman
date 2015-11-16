d7821e5171b36eb16103802611dfc047afeb495e
Yogthos <dmitri.sotnikov@gmail.com>
2015-11-16 13:03:00 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-dbcp "0.8.1"]
-                 [com.zaxxer/HikariCP "2.4.1"]
+                 [com.zaxxer/HikariCP "2.4.2"]
                  [yesql "0.5.1"]
                  [instaparse "1.4.1"]
                  [com.carouselapps/to-jdbc-uri "0.4.1"]
                  [org.clojure/java.jdbc "0.4.2"]]
   :profiles
   {:dev
-   {:dependencies [[com.h2database/h2 "1.4.187"]]}})
+   {:dependencies [[com.h2database/h2 "1.4.190"]]}})
