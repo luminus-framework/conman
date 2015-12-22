538b35cf0397bb5886f897b910951b8188e35b51
Yogthos <dmitri.sotnikov@gmail.com>
2015-12-22 10:52:56 -0500

    bumped up to-jdbc-uri

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conman "0.2.7"
+(defproject conman "0.2.8"
   :description "a database connection management library"
   :url "https://github.com/luminus-framework/conman"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.zaxxer/HikariCP "2.4.2"]
                  [yesql "0.5.1"]
-                 [com.carouselapps/to-jdbc-uri "0.4.1"]
+                 [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [org.clojure/java.jdbc "0.4.2"]]
   :profiles
   {:dev
