a73fa57cbe3f3cea6438d125cd816df582847980
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-01-31 18:17:36 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.zaxxer/HikariCP "2.4.2"]
+                 [com.zaxxer/HikariCP "2.4.3"]
                  [yesql "0.5.2"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [org.clojure/java.jdbc "0.4.2"]]
   :profiles
   {:dev
-   {:dependencies [[com.h2database/h2 "1.4.190"]]}})
+   {:dependencies [[com.h2database/h2 "1.4.191"]]}})
