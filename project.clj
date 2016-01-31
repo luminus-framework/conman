c879cbf7dc66ddbb1c8de8658a58f48a31cd0dcf
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-01-31 18:16:36 -0500

    bumped up yesql

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject conman "0.2.9"
+(defproject conman "0.3.0"
   :description "a database connection management library"
   :url "https://github.com/luminus-framework/conman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.zaxxer/HikariCP "2.4.2"]
-                 [yesql "0.5.1"]
+                 [yesql "0.5.2"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [org.clojure/java.jdbc "0.4.2"]]
   :profiles
