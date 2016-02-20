86ab48d53034e9a55652f21c13e66ca205aad78f
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-20 18:12:49 -0500

    bumped up HugSQL version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.zaxxer/HikariCP "2.4.3"]
-                 [com.layerware/hugsql "0.4.1-SNAPSHOT"]
+                 [com.layerware/hugsql "0.4.1"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [org.clojure/java.jdbc "0.4.2"]]
   :profiles
