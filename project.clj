2158522022eec9f4d6cf083deb1ac00fd894eef5
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-25 19:32:47 -0500

    bumped up hugsql

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject conman "0.4.4"
+(defproject conman "0.4.5"
   :description "a database connection management library"
   :url "https://github.com/luminus-framework/conman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.zaxxer/HikariCP "2.4.3"]
-                 [com.layerware/hugsql "0.4.2"]
+                 [com.layerware/hugsql "0.4.3"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [org.clojure/java.jdbc "0.4.2"]]
   :profiles
