55112f6c4f24c164df3b2ae7c86c59877e514590
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-05-23 08:14:30 -0400

    bumped up hikari-cp

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conman "0.5.8"
+(defproject conman "0.5.9"
   :description "a database connection management library"
   :url "https://github.com/luminus-framework/conman"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                  [com.layerware/hugsql "0.4.7"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [org.clojure/java.jdbc "0.6.1"]
-                 [hikari-cp "1.7.0"]]
+                 [hikari-cp "1.7.1"]]
   :profiles
   {:dev
    {:dependencies [[com.h2database/h2 "1.4.191"]]}})
