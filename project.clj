ff02690d9358e983dac6cab0a7f355ed07b25665
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-05-02 17:22:25 -0400

    bumped up hikari-cp

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conman "0.8.7"
+(defproject conman "0.8.8"
   :description "a database connection management library"
   :url "https://github.com/luminus-framework/conman"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.1"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [seancorfield/next.jdbc "1.0.409"]
-                 [hikari-cp "2.11.0"]]
+                 [hikari-cp "2.12.0"]]
   :profiles
   {:dev
    {:dependencies [[com.h2database/h2 "1.4.199"]
