b9502df9a15f0dabe8ad476c9081b710c6a12ed6
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-12-07 17:42:20 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject conman "0.9.5"
+(defproject conman "0.9.6"
   :description "a database connection management library"
   :url "https://github.com/luminus-framework/conman"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.layerware/hugsql-core "0.5.3"]
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]
                  [com.github.seancorfield/next.jdbc "1.2.796"]
-                 [hikari-cp "2.14.0"]]
+                 [hikari-cp "3.0.1"]]
   :profiles
   {:dev
    {:dependencies [[com.h2database/h2 "2.1.214"]
